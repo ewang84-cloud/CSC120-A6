90ced8f4701fddb525e72cdc3f3c7839e630ee7e
ewang84-cloud <ewang84@smith.edu>
2025-10-19 20:19:27 -0400

    a6-1

--- ShopTest.java
+++ ShopTest.java
@@ -3,6 +3,32 @@ import org.junit.Test;
 
 public class ShopTest {
 
-    // add your tests here 
-    
+@Test
+public void testComputerConstructor1(){
+    Computer C1 = new Computer("Mac Air", "ios18", 16, 6, "IOS", 2022, 899);
+    assertEquals("Mac Air", C1.description);
+    assertEquals("ios18", C1.processorType);
+    assertEquals(16, C1.hardDriveCapacity);
+    assertEquals(6, C1.memory);
+    assertEquals("IOS", C1.operatingSystem);
+    assertEquals(2022, C1.yearMade);
+    assertEquals(899, C1.price);
+} 
+
+@Test
+public void testComputerConstructorPrice(){
+    Computer C1 = new Computer("Mac Air", "ios18", 16, 6, "IOS", 2022, 899);
+    assertEquals(899, C1.price);
+}
+
+@Test
+public void testSetOS(){
+    Computer C1 = new Computer("Mac Air", "ios18", 16, 6, "IOS", 2022, 899);
+    C1.setOS("IOS-2");
+    assertEquals("IOS-2", C1.operatingSystem);
+}
+
+@Test
+public void test
+
 }
--- bugLog.md
+++ bugLog.md
@@ -1,14 +1,14 @@
 ## Bug 1
-Brief description:  
-Failed unit test: 
+Brief description:  The constuctor of Computer class hardcode the memory of new creator computeras 16 instead of the input parameter
+Failed unit test: testComputerConstructor
 
 ## Bug 2
-Brief description:  
-Failed unit test: 
+Brief description:  The constuctor of Computer class hardcode the price of new creator computer as 0 instead of the input parameter
+Failed unit test: testComputerConstructorPrice
 
 ## Bug 3
-Brief description:  
-Failed unit test: 
+Brief description:  The SetOS method in Computer Class is not working, it will not change the operating system into the input new OS, it will always change into "none"
+Failed unit test: testSetOS
 
 ## Bug 4
 Brief description:  
